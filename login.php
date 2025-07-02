64e19e806df08ea3247c5243255fbcf3fb39341e
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-03 01:34:30 +0600

    io

--- login.php
+++ login.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Login</title>
 </head>
 <body>
-    
+<div class="login_header_area">
+    <h2>Network</h2>
+</div>    
+
+
+<div class="login_form_area">
+    <div class="sub_form">
+    <form action="" method="post"> 
+        <h3>Login here</h3>
+        <input type="text" name="username" id="" placeholder="Username" required><br>
+        <input type="password" name="password" id="" placeholder="Password" required><br>
+        <input type="submit" value="Login">
+    </form>
+    </div>
+</div>
+
+
+
+
+
 </body>
 </html>
