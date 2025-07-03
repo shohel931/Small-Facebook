e68b5edda82af333c3c37764f49371e3cda73990
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-03 16:09:48 +0600

    ff

--- login.php
+++ login.php
@@ -4,24 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Login</title>
+    <title>Network - log in or sign up</title>
 </head>
 <body>
-<div class="login_header_area">
-    <h2>Network</h2>
-</div>    
-
 
 <div class="login_form_area">
+    <div class="sub_text">
+        <h2>Network</h2>
+        <p>Network helps you connect and share with the people in your life.</p>
+    </div>
     <div class="sub_form">
-    <form action="" method="post"> 
-        <h3>Login in to network</h3>
+    <form action="" method="post">
         <input type="text" name="username" id="" placeholder="Username" required><br>
         <input type="password" name="password" id="" placeholder="Password" required><br>
         <input type="submit" value="Log in"><br>
         <a href="#">Forgotten password?</a>
     </form>
-    
     </div>
 </div>
 
