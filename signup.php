9678337e5f89861cf0eb3333c8293ce6937c943b
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-04 07:47:37 +0600

    c

--- signup.php
+++ signup.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Network - log in or sign up</title>
+    <title>Network - sign up</title>
 </head>
 <body>
 
+<div class="title_signup">
+    <h2 style="margin-top: 20px !important; margin-bottom: -50px !important;" class="hhh">Network</h2>
+</div>
+
 <div class="login_form_area">
     <div class="sub_form">
+        <h3 style="margin-top: -10px; font-size: 21px;">Create a new account</h3>
+        <hr>
     <form action="" method="post">
         <input type="text" name="fname" id="taxt" placeholder="First name" required><br>
         <input type="text" name="lname" id="taxt" placeholder="Last name (optional)" ><br>
@@ -22,7 +28,6 @@
             </select>
         <input type="text" name="username" id="taxt" placeholder="Username" required><br>
         <input type="email" name="email" id="taxt" placeholder="Email" required><br>
-        <input type="number" name="number" id="taxt" placeholder="Number" required><br>
         <input type="password" name="password" id="taxt" placeholder="New password" required><br>
         <input type="submit" value="Sign up"><br>
     </form><br>
