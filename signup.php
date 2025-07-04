a921407034b2e4ee71d90f04efd41ddf757b0906
MD. Shohel Rana <shohel065931@gmail.com>
2025-07-04 07:01:47 +0600

    rt

--- signup.php
+++ signup.php
@@ -13,78 +13,17 @@
     <form action="" method="post">
         <input type="text" name="fname" id="taxt" placeholder="First name" required><br>
         <input type="text" name="lname" id="taxt" placeholder="Last name (optional)" ><br>
-        <span style="text-align: left !important;">Date of birth: <br>
-            <select  name="" id="date">
-                <option value="1">1</option>
-                <option value="1">2</option>
-                <option value="1">3</option>
-                <option value="1">4</option>
-                <option value="1">5</option>
-                <option value="1">6</option>
-                <option value="1">7</option>
-                <option value="1">8</option>
-                <option value="1">9</option>
-                <option value="1">10</option>
-                <option value="1">11</option>
-                <option value="1">12</option>
-                <option value="1">13</option>
-                <option value="1">14</option>
-                <option value="1">15</option>
-                <option value="1">16</option>
-                <option value="1">17</option>
-                <option value="1">18</option>
-                <option value="1">19</option>
-                <option value="1">20</option>
-                <option value="1">21</option>
-                <option value="1">22</option>
-                <option value="1">23</option>
-                <option value="1">24</option>
-                <option value="1">25</option>
-                <option value="1">26</option>
-                <option value="1">27</option>
-                <option value="1">28</option>
-                <option value="1">29</option>
-                <option value="1">30</option>
-                <option value="1">31</option>
-            </select>
-            <select  name="" id="date">
-                <option value="1">1</option>
-                <option value="1">2</option>
-                <option value="1">3</option>
-                <option value="1">4</option>
-                <option value="1">5</option>
-                <option value="1">6</option>
-                <option value="1">7</option>
-                <option value="1">8</option>
-                <option value="1">9</option>
-                <option value="1">10</option>
-                <option value="1">11</option>
-                <option value="1">12</option>
-            </select>
-            <select  name="" id="date">
-                <option value="1">2000</option>
-                <option value="1">2001</option>
-                <option value="1">2002</option>
-                <option value="1">2003</option>
-                <option value="1">2004</option>
-                <option value="1">2005</option>
-                <option value="1">2006</option>
-                <option value="1">2007</option>
-                <option value="1">2008</option>
-                <option value="1">2009</option>
-                <option value="1">2010</option>
-                <option value="1">2011</option>
-            </select>
-        </span><br>
-      <span class="gander"> Gander:</span> <br>
-          <select  name="" id="gander">
+       
+          <select  name="" id="date">
                 <option value="1">Male</option>
                 <option value="1">Female</option>
                 <option value="1">Custom</option>
             </select>
         <input type="text" name="username" id="taxt" placeholder="Username" required><br>
+        <input type="email" name="email" id="taxt" placeholder="Email" required><br>
+        <input type="number" name="number" id="taxt" placeholder="Number" required><br>
         <input type="password" name="password" id="taxt" placeholder="Password" required><br>
-        <input type="submit" value="Log in"><br>
+        <input type="submit" value="Sign up"><br>
     </form><br>
         <a class="already" href="login.php">Already have an account?</a>
     </div>
